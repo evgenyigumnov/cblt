94a90c911c9b922983a46191c2119e1184d7cc92
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-11-10 10:59:34 +0500

    0.0.5

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t ievkz/cblt:latest . && \
-docker build -t ievkz/cblt:0.0.4 . && \
+docker build -t ievkz/cblt:0.0.5 . && \
 docker push ievkz/cblt:latest && \
-docker push ievkz/cblt:0.0.4
+docker push ievkz/cblt:0.0.5
 cargo publish -p cblt --allow-dirty
