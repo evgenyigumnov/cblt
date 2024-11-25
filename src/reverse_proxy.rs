e026ad11750667fd434727ce7c36439880e68e6f
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-11-25 16:27:16 +0500

    websocket

--- src/reverse_proxy.rs
+++ src/reverse_proxy.rs
@@ -4,7 +4,7 @@ use bytes::BytesMut;
 use http::{Request, Response, StatusCode};
 use log::debug;
 use reqwest::Client;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
 #[cfg(feature = "trace")]
 use tracing::instrument;
 pub const HEAPLESS_STRING_SIZE: usize = 100;
@@ -36,43 +36,98 @@ where
                 #[cfg(debug_assertions)]
                 debug!("Destination URI: {}", dest_uri);
 
-                let mut req_builder =
-                    client_reqwest.request(request.method().clone(), dest_uri.as_str());
-                for (key, value) in request.headers().iter() {
-                    req_builder = req_builder.header(key, value);
-                }
-                let body = request.body();
-                if !body.is_empty() {
-                    req_builder = req_builder.body(body.to_vec());
-                }
-
-                match req_builder.send().await {
-                    Ok(resp) => {
-                        let status = resp.status();
-                        let headers = resp.headers();
-                        let mut response_builder = Response::builder().status(status);
-                        for (key, value) in headers.iter() {
-                            response_builder = response_builder.header(key, value);
-                        }
-                        let mut stream = resp.bytes_stream();
-                        let response = response_builder.body("")?;
-                        send_response_stream(socket, response, request, &mut stream).await?;
-                        if status != StatusCode::OK {
-                            return Err(CbltError::ResponseError {
-                                details: "Bad gateway".to_string(),
-                                status_code: status,
-                            });
-                        } else {
-                            return Ok(status);
-                        }
-                    }
-                    Err(err) => {
-                        #[cfg(debug_assertions)]
-                        debug!("Error: {:?}", err);
-                        return Err(CbltError::ResponseError {
-                            details: err.to_string(),
+                if is_websocket_upgrade(request) {
+                    // Handle WebSocket upgrade request
+                    // Establish a TCP connection to the backend
+                    let dest_uri_parsed = dest_uri.parse::<http::Uri>()
+                        .map_err(|e| CbltError::ResponseError {
+                            details: e.to_string(),
                             status_code: StatusCode::BAD_GATEWAY,
-                        });
+                        })?;
+                    let host = dest_uri_parsed.host().ok_or(CbltError::ResponseError {
+                        details: "Invalid destination URI".to_string(),
+                        status_code: StatusCode::BAD_GATEWAY,
+                    })?;
+                    let port = dest_uri_parsed.port_u16().unwrap_or(80); // Adjust if needed for https
+                    let backend_addr = format!("{}:{}", host, port);
+                    debug!("Connecting to backend at {}", backend_addr);
+                    let mut backend_stream = TcpStream::connect(backend_addr).await.map_err(|e| CbltError::ResponseError {
+                        details: e.to_string(),
+                        status_code: StatusCode::BAD_GATEWAY,
+                    })?;
+
+                    // Send the initial request to the backend
+                    let request_bytes = request_to_bytes(request)?;
+                    backend_stream.write_all(&request_bytes).await.map_err(|e| CbltError::ResponseError {
+                        details: e.to_string(),
+                        status_code: StatusCode::BAD_GATEWAY,
+                    })?;
+
+                    // Read the response from the backend
+                    let mut backend_buf = BytesMut::with_capacity(8192);
+                        let response = socket_to_response(&mut backend_stream, &mut backend_buf).await?;
+
+                    // Send the response back to the client
+                    socket.write_all(&backend_buf).await.map_err(|e| CbltError::ResponseError {
+                        details: e.to_string(),
+                        status_code: StatusCode::BAD_GATEWAY,
+                    })?;
+
+                    // If the response is 101 Switching Protocols, start relaying data
+                    if response.status() == StatusCode::SWITCHING_PROTOCOLS {
+                        // Relay data between client and backend
+                        let (mut backend_read_half, mut backend_write_half) = backend_stream.split();
+                        let (mut client_read_half, mut client_write_half) =  tokio::io::split(socket);
+                        let client_to_backend = tokio::io::copy(&mut client_read_half, &mut backend_write_half);
+                        let backend_to_client = tokio::io::copy(&mut backend_read_half, &mut client_write_half);
+                        tokio::try_join!(client_to_backend, backend_to_client).map_err(|e| CbltError::ResponseError {
+                            details: e.to_string(),
+                            status_code: StatusCode::BAD_GATEWAY,
+                        })?;
+                        return Ok(StatusCode::SWITCHING_PROTOCOLS);
+                    } else {
+                        // Not a successful WebSocket handshake
+                        return Ok(response.status());
+                    }
+                } else {
+                    let mut req_builder =
+                        client_reqwest.request(request.method().clone(), dest_uri.as_str());
+                    for (key, value) in request.headers().iter() {
+                        req_builder = req_builder.header(key, value);
+                    }
+                    let body = request.body();
+                    if !body.is_empty() {
+                        req_builder = req_builder.body(body.to_vec());
+                    }
+
+                    match req_builder.send().await {
+                        Ok(resp) => {
+                            let status = resp.status();
+                            let headers = resp.headers();
+                            let mut response_builder = Response::builder().status(status);
+                            for (key, value) in headers.iter() {
+                                response_builder = response_builder.header(key, value);
+                            }
+                            let mut stream = resp.bytes_stream();
+                            let response = response_builder.body("")?;
+                            send_response_stream(socket, response, request, &mut stream).await?;
+                            if status != StatusCode::OK {
+                                return Err(CbltError::ResponseError {
+                                    details: "Bad gateway".to_string(),
+                                    status_code: status,
+                                });
+                            } else {
+                                return Ok(status);
+                            }
+                        }
+                        Err(err) => {
+                            #[cfg(debug_assertions)]
+                            debug!("Error: {:?}", err);
+                            return Err(CbltError::ResponseError {
+                                details: err.to_string(),
+                                status_code: StatusCode::BAD_GATEWAY,
+                            });
+                        }
                     }
                 }
             } else {
@@ -87,12 +142,107 @@ where
     Err(CbltError::DirectiveNotMatched)
 }
 
+fn is_websocket_upgrade(request: &Request<BytesMut>) -> bool {
+    if let Some(connection_header) = request.headers().get("Connection") {
+        if connection_header.to_str().map(|s| s.to_ascii_lowercase().contains("upgrade")).unwrap_or(false) {
+            if let Some(upgrade_header) = request.headers().get("Upgrade") {
+                if upgrade_header.to_str().map(|s| s.to_ascii_lowercase() == "websocket").unwrap_or(false) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+
+fn request_to_bytes(request: &Request<BytesMut>) -> Result<Vec<u8>, CbltError> {
+    let mut buf = Vec::new();
+    // Write request line
+    buf.extend_from_slice(request.method().as_str().as_bytes());
+    buf.extend_from_slice(b" ");
+    buf.extend_from_slice(request.uri().path_and_query().map(|pq| pq.as_str()).unwrap_or("/").as_bytes());
+    buf.extend_from_slice(b" HTTP/1.1\r\n");
+
+    // Write headers
+    for (key, value) in request.headers() {
+        buf.extend_from_slice(key.as_str().as_bytes());
+        buf.extend_from_slice(b": ");
+        buf.extend_from_slice(value.as_bytes());
+        buf.extend_from_slice(b"\r\n");
+    }
+    buf.extend_from_slice(b"\r\n");
+
+    // Write body
+    buf.extend_from_slice(&request.body());
+
+    Ok(buf)
+}
+
+async fn socket_to_response<S>(socket: &mut S, buf: &mut BytesMut) -> Result<Response<()>, CbltError>
+    where
+        S: AsyncReadExt + Unpin,
+{
+    loop {
+        let bytes_read = socket.read_buf(buf).await.unwrap_or(0);
+        if bytes_read == 0 {
+            break;
+        }
+        // Try to parse the response
+        let mut headers = [httparse::EMPTY_HEADER; 32];
+        let mut res = httparse::Response::new(&mut headers);
+
+        match res.parse(buf) {
+            Ok(httparse::Status::Complete(header_len)) => {
+                let status_code = res.code.ok_or(CbltError::ResponseError {
+                    details: "Failed to parse status code".to_string(),
+                    status_code: StatusCode::BAD_GATEWAY,
+                })?;
+                let status = StatusCode::from_u16(status_code).map_err(|e| CbltError::ResponseError {
+                    details: e.to_string(),
+                    status_code: StatusCode::BAD_GATEWAY,
+                })?;
+                let version = res.version.ok_or(CbltError::ResponseError {
+                    details: "Failed to parse HTTP version".to_string(),
+                    status_code: StatusCode::BAD_GATEWAY,
+                })?;
+                let mut response_builder = Response::builder().status(status);
+                for header in res.headers {
+                    response_builder = response_builder.header(header.name, header.value);
+                }
+                let response = response_builder.body(()).map_err(|e| CbltError::ResponseError {
+                    details: e.to_string(),
+                    status_code: StatusCode::BAD_GATEWAY,
+                })?;
+                return Ok(response);
+            }
+            Ok(httparse::Status::Partial) => {
+                // Need to read more data
+                continue;
+            }
+            Err(e) => {
+                return Err(CbltError::ResponseError {
+                    details: e.to_string(),
+                    status_code: StatusCode::BAD_GATEWAY,
+                });
+            }
+        }
+    }
+
+    Err(CbltError::ResponseError {
+        details: "Failed to read response from backend".to_string(),
+        status_code: StatusCode::BAD_GATEWAY,
+    })
+}
+
+
 use crate::config::LoadBalancePolicy;
 use std::collections::HashMap;
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use tokio::net::TcpStream;
 use tokio::sync::RwLock;
 
 #[derive(Debug, Clone)]
